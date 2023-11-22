34efa29fa420822bd197a66e7fbc79b9be37092a
Dmytro <srk90512@gmail.com>
2023-11-22 14:33:25 +0200

    rewrite to dynamic index.php

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
-<?php 
+<?php
 // 
 // Main template file
 // 
 // @package Aquila
-// 
+//
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WordPress Theme</title>
-</head>
-<body>
-    
-</body>
-</html>
+<div class="content">
+    Content
+</div>
 
+
+<?php
+get_footer();
+?>
