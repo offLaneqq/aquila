a6405a6dab0826c985f6beb648b8547dd4df6886
Dmytro <srk90512@gmail.com>
2023-12-30 00:47:27 +0200

    call all template part to correctly display needed information of posts on blog page

---
+++ template-parts/content.php
@@ -0,0 +1,12 @@
+<?php
+// 
+// Content template
+// 
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('mb-5'); ?>>
+    <?php get_template_part('template-parts/components/blog/entry-header') ?>
+    <?php get_template_part('template-parts/components/blog/entry-meta') ?>
+    <?php get_template_part('template-parts/components/blog/entry-content') ?>
+    <?php get_template_part('template-parts/components/blog/entry-footer') ?>
+</article>
