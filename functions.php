fbe6589758809007676691a9085d22cd1b6ec995
Dmytro <srk90512@gmail.com>
2023-12-30 00:29:24 +0200

    update function of connection styles and script by class way

--- functions.php
+++ functions.php
@@ -5,31 +5,24 @@
 // @package Aquila
 // 
 
+use AQUILA_THEME\Inc\AQUILA_THEME;
+
 if (!defined('AQUILA_DIR_PATH')) {
     define('AQUILA_DIR_PATH', untrailingslashit(get_template_directory()));
 }
 
-
-require_once AQUILA_DIR_PATH.'/inc/helpers/autoloader.php';
-// use AQUILA_THEME\Inc\Traits\Singleton as Singleton;
-
-function aquila_enqueue_scripts()
-{
-    // Register Styles
-    wp_register_style('style', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'));
-    wp_register_style('bootstrap', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false);
-
-    // Register Scripts
-    wp_register_script('main', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
-    wp_register_script('bootstrap', get_template_directory_uri() . '/assets/src/library/js/bootstrap.bundle.min.js', ['jquery'], false, true);
-
-    // Enqueue Styles
-    wp_enqueue_style('style');
-    wp_enqueue_style('bootstrap');
-
-    // Enqueue Scripts
-    wp_enqueue_script('main');
-    wp_enqueue_script('bootstrap');
+if (!defined('AQUILA_DIR_URI')) {
+    define('AQUILA_DIR_URI', untrailingslashit(get_template_directory_uri()));
 }
+
+require_once AQUILA_DIR_PATH . '/inc/helpers/autoloader.php';
+require_once AQUILA_DIR_PATH . '/inc/helpers/template-tags.php';
+
+function aquila_get_theme_instance()
+{
+    AQUILA_THEME::get_instance();
+}
+
+aquila_get_theme_instance();
 
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
