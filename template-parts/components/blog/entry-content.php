06c615ec64b2aaa319bde12d44ee699f8385dd2d
Dmytro <srk90512@gmail.com>
2024-01-01 22:37:01 +0200

    template for excerpt

---
+++ template-parts/components/blog/entry-content.php
@@ -0,0 +1,27 @@
+<?php
+
+// Template for entry content
+
+?>
+
+<div class="entry-content">
+    <?php
+    if (is_single()) {
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('Continue reading %s <span class="meta-nav">rarr;</span>', 'aquila'),
+                    [
+                        "span" => [
+                            "class" => []
+                        ]
+                    ]
+                ),
+                the_title('<span class="screen-reader-text">"', '"</span>', false)
+            )
+        );
+    } else {
+        aquila_the_excerpt(350);
+    }
+    ?>
+</div>
