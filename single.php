73ff384ed55d2568e918f48e4c8c4116a7fceb5c
Dmytro <srk90512@gmail.com>
2024-01-02 19:12:51 +0200

    create template for single post(when user open more info ab/ the post)

--- single.php
+++ single.php
@@ -1,13 +1,42 @@
 <?php
 // 
-// Template for post
+// Template for single post
 // 
 
 get_header();
 ?>
 
-<div class="content">
-    <?php esc_html_e('Single Post', 'aquila'); ?>
+<div id="primary">
+    <main class="site-main mt-5" id="main" role="main">
+        <?php if (have_posts()): ?>
+            <div class="container">
+
+                <?php
+                if (is_home() && !is_front_page()) {
+                    ?>
+                    <header class="mb-5">
+                        <h1 class="page-title">
+                            <?php single_post_title(); ?>
+                        </h1>
+                    </header>
+                    <?php
+                }
+                // Start loop
+                while (have_posts()):
+                    the_post();
+
+                    get_template_part('template-parts/content');
+
+                endwhile ?>
+            </div>
+    </div>
+    <?php
+        else: // can be bug here
+            get_template_part('template-parts/content-none');
+
+        endif;
+        ?>
+</main>
 </div>
 
 
