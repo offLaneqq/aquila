d50af686b3b8eec2051a3695a2fda3baa9e8f19b
Dmytro <srk90512@gmail.com>
2024-01-03 04:20:08 +0200

    add grid system and resize area for post info and widgets

--- single.php
+++ single.php
@@ -8,42 +8,49 @@ get_header();
 
 <div id="primary">
     <main class="site-main mt-5" id="main" role="main">
-        <?php if (have_posts()): ?>
-            <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-12">
+                    <?php if (have_posts()): ?>
+                        <div class="post-wrap">
 
-                <?php
-                if (is_home() && !is_front_page()) {
+                            <?php
+                            if (is_home() && !is_front_page()) {
+                                ?>
+                                <header class="mb-5">
+                                    <h1 class="page-title">
+                                        <?php single_post_title(); ?>
+                                    </h1>
+                                </header>
+                                <?php
+                            }
+                            // Start loop
+                            while (have_posts()):
+                                the_post();
+
+                                get_template_part('template-parts/content');
+
+                            endwhile ?>
+                        </div>
+                        <?php
+                    else: // can be bug here
+                        get_template_part('template-parts/content-none');
+
+                    endif;
                     ?>
-                    <header class="mb-5">
-                        <h1 class="page-title">
-                            <?php single_post_title(); ?>
-                        </h1>
-                    </header>
-                    <?php
-                }
-                // Start loop
-                while (have_posts()):
-                    the_post();
-
-                    get_template_part('template-parts/content');
-
-                endwhile ?>
+                    <div class="prev-link">
+                        <?php previous_post_link(); ?>
+                    </div>
+                    <div class="next-link">
+                        <?php next_post_link(); ?>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12">
+                    <?php get_sidebar(); ?>
+                </div>
             </div>
-    </div>
-    <?php
-        else: // can be bug here
-            get_template_part('template-parts/content-none');
-
-        endif;
-        ?>
-<div class="container">
-    <?php 
-        previous_post_link();
-        echo ' ----- ';
-        next_post_link();
-    ?>
-</div>
-</main>
+        </div>
+    </main>
 </div>
 
 
