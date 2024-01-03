75734b1d05273b80a2f7e009b9b49a71864dfa76
Dmytro <srk90512@gmail.com>
2024-01-03 04:21:51 +0200

    add widgets for footer

--- footer.php
+++ footer.php
@@ -7,8 +7,18 @@
 </div>
 </div>
 <footer>
-    Footer
+    <h3>Footer</h3>
+    <?php
+    if (is_active_sidebar('sidebar-2')) {
+        ?>
+        <aside>
+            <?php dynamic_sidebar('sidebar-2') ?>
+        </aside>
+        <?php
+    }
+    ?>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
