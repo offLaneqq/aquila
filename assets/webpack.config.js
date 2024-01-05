934e3b7d68541853cdc73b199b0dcf7c0b616d7c
Dmytro <srk90512@gmail.com>
2024-01-06 00:59:17 +0200

    some changes + need to fix bug with fonts(saved in main folder build, not in folder fonts)

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,17 +1,20 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");   //remove all unused webpacks on rebuild
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); //remove all unused webpacks on rebuild
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const cssnano = require("cssnano");            // minimize css file(remove all comments)
+const cssnano = require("cssnano"); // minimize css file(remove all comments)
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const JS_DIR = path.resolve(__dirname, "src/js");
 const IMG_DIR = path.resolve(__dirname, "src/img");
+const LIB_DIR = path.resolve(__dirname, "src/library");
 const BUILD_DIR = path.resolve(__dirname, "build");
 
 const entry = {
   main: JS_DIR + "/main.js",
   single: JS_DIR + "/single.js",
+  editor: JS_DIR + "/editor.js",
 };
 
 const output = {
@@ -19,15 +22,18 @@ const output = {
   filename: "js/[name].js", // from js/main.js to build/main.js
 };
 
-
 const plugins = (argv) => [
-    new CleanWebpackPlugin({
-        cleanStaleWebpackAssets: (argv.mode === "production")     // clean if production
-    }),
+  new CleanWebpackPlugin({
+    cleanStaleWebpackAssets: argv.mode === "production", // clean if production
+  }),
 
-    new MiniCssExtractPlugin({
-        filename: "css/[name].css"                       // extracted in build/css/main.css for example
-    })
+  new MiniCssExtractPlugin({
+    filename: "css/[name].css", // extracted in build/css/main.css for example
+  }),
+
+  new CopyPlugin({
+    patterns: [{ from: LIB_DIR, to: BUILD_DIR + "/library" }],
+  }),
 ];
 
 const rules = [
@@ -43,18 +49,30 @@ const rules = [
     use: [
       MiniCssExtractPlugin.loader, // convert css into array of js, then minicssextraxtplugin take info and extract it into files
       "css-loader",
-      "sass-loader"], 
+      "sass-loader",
+    ],
   },
   {
     test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
     use: {
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]",
-          publicPath: "production" === process.env.NODE_ENV ? "../" : "../../",
-        },
+      loader: "file-loader",
+      options: {
+        name: "[path][name].[ext]",
+        publicPath: "production" === process.env.NODE_ENV ? "../" : "../../",
       },
+    },
   },
+  // {
+  //   test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+  //   exclude: [IMG_DIR, /node_modules/],
+  //   use: {
+  //     loader: "file-loader",
+  //     options: {
+  //       name: "[path][name].[ext]",
+  //       publicPath: "production" === process.env.NODE_ENV ? "../" : "../../",
+  //     },
+  //   },
+  // },
 ];
 
 module.exports = (env, argv) => ({
@@ -73,12 +91,11 @@ module.exports = (env, argv) => ({
         cache: false,
         parallel: true,
         sourceMap: false,
-
-      })
-    ]
+      }),
+    ],
   },
   plugins: plugins(argv),
   externals: {
-    jquery: 'jQuery',
-  }
+    jquery: "jQuery",
+  },
 });
