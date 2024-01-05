244f9ddf2967030e71841897c5f5683bbfe13696
Dmytro <srk90512@gmail.com>
2024-01-05 04:43:25 +0200

    fix paths and remove line from header? idk xd

--- header.php
+++ header.php
@@ -20,8 +20,8 @@
         wp_body_open();
     } ?>
 
-<div id="page" class="site">
-    <header id="masthead" class="site-header" role="banner">
-        <?php get_template_part('template-parts/header/nav') ?>
-    </header>
-    <div id="content" class="site-content">
+    <div id="page" class="site">
+        <header id="masthead" class="site-header" role="banner">
+            <?php get_template_part('template-parts/header/nav') ?>
+        </header>
+        <div id="content" class="site-content">
