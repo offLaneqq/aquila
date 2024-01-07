be4ab0eaa403a4ffa6aaf8d219bfeba297973f03
Dmytro <srk90512@gmail.com>
2024-01-07 23:55:17 +0200

    Add content for second custom pattern

---
+++ template-parts/patterns/two-columns.php
@@ -0,0 +1,55 @@
+<?php
+// 
+// Two Columns Block Patterns template
+// 
+?>
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide"><!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+    <div class="wp-block-columns alignwide">
+        <!-- wp:column {"verticalAlignment":"center","width":"100%","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black","textColor":"white","className":"aquila-two-columns-pattern"} -->
+        <div class="wp-block-column is-vertically-aligned-center aquila-two-columns-pattern has-white-color has-black-background-color has-text-color has-background has-link-color"
+            style="flex-basis:100%"><!-- wp:columns -->
+            <div class="wp-block-columns"><!-- wp:column -->
+                <div class="wp-block-column">
+                    <!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|luminous-vivid-orange"}}}},"textColor":"luminous-vivid-orange"} -->
+                    <h2 class="wp-block-heading has-luminous-vivid-orange-color has-text-color has-link-color">Header
+                        One
+                    </h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph -->
+                    <p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry.
+                        Lorem
+                        Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                        took
+                        a galley of type and scrambled it to make a type specimen book. </p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+
+                <!-- wp:column -->
+                <div class="wp-block-column">
+                    <!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|luminous-vivid-orange"}}}},"textColor":"luminous-vivid-orange"} -->
+                    <h2 class="wp-block-heading has-luminous-vivid-orange-color has-text-color has-link-color">Header
+                        Two
+                    </h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph -->
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page
+                        when
+                        looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                        distribution of letters, as opposed to using 'Content here, content here', making it look like
+                        readable English.&nbsp;</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
