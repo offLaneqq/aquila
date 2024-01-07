a1889b56024d2aea69f0b0e2012214ba268e040a
Dmytro <srk90512@gmail.com>
2024-01-07 02:07:01 +0200

    remove all default patterns and call class of Block_Patterns

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -15,6 +15,7 @@ class AQUILA_THEME
         Menus::get_instance();
         Meta_Boxes::get_instance();
         Sidebars::get_instance();
+        Block_Patterns::get_instance();
 
         $this->setup_hooks();
 
@@ -56,8 +57,11 @@ class AQUILA_THEME
         add_theme_support('wp-block-styles');
         add_theme_support('align-wide');
         add_theme_support('editor-styles');
-        add_editor_style('assets/build/css/editor.css');
         add_theme_support('post-thumbnails');
+        add_editor_style('assets/build/css/editor.css');
+
+        // Remove the core block patterns
+        remove_theme_support('core-block-patterns');
 
         // Register image size
         add_image_size('featured-thumbnail', 350, 233, true);
