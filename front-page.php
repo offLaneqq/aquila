db8334915b585d8ec1505dc303f8dda9bb3d5dbc
Dmytro <srk90512@gmail.com>
2024-01-07 23:56:49 +0200

    call front-page(homepage) template

--- front-page.php
+++ front-page.php
@@ -1,19 +1,35 @@
-<?php 
+<?php
 
 // 
 // Front page template
 // 
 
 get_header();
-
 ?>
 
-<div>
-    Front page
+<div id="primary">
+    <main class="site-main mt-5" id="main" role="main">
+        <div class="home-page-wrap">
+            <?php if (have_posts()):
+                while (have_posts()):
+                    the_post();
+
+                    get_template_part('template-parts/content', 'page');
+
+                endwhile ?>
+
+                <?php
+            else: // can be bug here
+                get_template_part('template-parts/content-none');
+
+            endif;
+            ?>
+        </div>
+    </main>
 </div>
 
-<?php 
 
+
+<?php
 get_footer();
-
 ?>
