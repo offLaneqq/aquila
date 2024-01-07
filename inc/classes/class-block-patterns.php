a17324c68903203659b07ca499fc5ed7475a0f49
Dmytro <srk90512@gmail.com>
2024-01-07 23:55:35 +0200

    call patterns with contents

--- inc/classes/class-block-patterns.php
+++ inc/classes/class-block-patterns.php
@@ -28,37 +28,56 @@ class Block_Patterns
     public function register_block_patterns()
     {
         if (function_exists('register_block_pattern')) {
+
+            // Cover Pattern
+            $cover_content = $this->get_pattern_content('template-parts/patterns/cover');
+
             register_block_pattern(
                 'aquila/cover',
                 [
                     'title' => __('Aquila Cover', 'aquila'),
                     'description' => __('Aquila Cover block with image and text', 'aquila'),
                     'categories' => ['cover'],
-                    'content' => '<!-- wp:cover {"url":"http://test-site.local/wp-content/uploads/2023/12/nothin-to-see-here-neon-sign.jpeg","id":648,"dimRatio":50,"align":"full","className":"aquila-cover","layout":{"type":"constrained"}} -->
-                    <div class="wp-block-cover alignfull aquila-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-648" alt="Nothin\' to See Here Neon Sign" src="http://test-site.local/wp-content/uploads/2023/12/nothin-to-see-here-neon-sign.jpeg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1} -->
-                    <h1 class="wp-block-heading has-text-align-center"><strong>Nothin\' to see here...</strong></h1>
-                    <!-- /wp:heading -->
-                    
-                    <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|cyan-bluish-gray"}}}},"textColor":"cyan-bluish-gray"} -->
-                    <p class="has-text-align-center has-cyan-bluish-gray-color has-text-color has-link-color">Lillian and Madison were unlikely roommates and yet inseparable friends at their elite boarding school.</p>
-                    <!-- /wp:paragraph -->
-                    
-                    <!-- wp:buttons {"layout":{"type":"flex","verticalAlignment":"center","justifyContent":"center"}} -->
-                    <div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","textColor":"cyan-bluish-gray","style":{"elements":{"link":{"color":{"text":"var:preset|color|cyan-bluish-gray"}}},"border":{"radius":"17px"}},"className":"is-style-outline"} -->
-                    <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-cyan-bluish-gray-color has-text-color has-link-color has-text-align-center wp-element-button" style="border-radius:17px">Blogs</a></div>
-                    <!-- /wp:button --></div>
-                    <!-- /wp:buttons --></div></div>
-                    <!-- /wp:cover -->'
+                    'content' => $cover_content
+                ]
+            );
+
+            // Two Columns Pattern
+            $two_columns_content = $this->get_pattern_content('template-parts/patterns/two-columns');
+
+            register_block_pattern(
+                'aquila/two-columns',
+                [
+                    'title' => __('Aquila Two Columns', 'aquila'),
+                    'description' => __('Aquila Cover block with heading and text', 'aquila'),
+                    'categories' => ['two_columns'],
+                    'content' => $two_columns_content
                 ]
             );
         }
+    }
+
+    public function get_pattern_content($template_part)
+    {
+        // Function get_template_part echo data. 
+        // To resolve this call ob_start to don't go to echo out. 
+        // Variable cover_content save data about code of pattern(template-parts/patterns/cover.php)
+        // Also after call ob_end_clear()
+        ob_start();
+
+        get_template_part($template_part);
+        $pattern_content = ob_get_contents();
+
+        ob_end_clean();
+
+        return $pattern_content;
     }
 
     public function register_block_pattern_categories()
     {
         $pattern_categories = [
             'cover' => __('Cover', 'aquila'),
-            'carousel' => __('Carousel', 'aquila')
+            'two_columns' => __('Two Columns', 'aquila'),
         ];
 
         if (!empty($pattern_categories) && is_array($pattern_categories)) {
