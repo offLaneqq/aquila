7d86bd3a49fdfd40a85b3f38d5944a785382d22f
Dmytro <srk90512@gmail.com>
2024-01-21 23:15:17 +0200

    rebuild

--- assets/build/js/editor.js
+++ assets/build/js/editor.js
@@ -2,9 +2,9 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/sass/blocks.scss":
+/***/ "./src/sass/editor.scss":
 /*!******************************!*\
-  !*** ./src/sass/blocks.scss ***!
+  !*** ./src/sass/editor.scss ***!
   \******************************/
 /***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
@@ -57,12 +57,12 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 !function() {
 /*!**************************!*\
-  !*** ./src/js/blocks.js ***!
+  !*** ./src/js/editor.js ***!
   \**************************/
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _sass_blocks_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/blocks.scss */ "./src/sass/blocks.scss");
+/* harmony import */ var _sass_editor_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/editor.scss */ "./src/sass/editor.scss");
 
 }();
 /******/ })()
 ;
-//# sourceMappingURL=blocks.js.map
+//# sourceMappingURL=editor.js.map
