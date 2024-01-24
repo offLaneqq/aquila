48da9d0f586ac9710373874eee9ee5d4a7476185
Dmytro <srk90512@gmail.com>
2024-01-24 23:01:08 +0200

    replace search form for WP hierarchy to searchform.php

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -74,10 +74,7 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
                 <?php
                 }
                 ?>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                <?php get_search_form(); ?>
             </div>
         </div>
     </div>
