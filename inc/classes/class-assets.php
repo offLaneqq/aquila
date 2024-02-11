aae1c1f2db84256155708451289ffd3d39bfd955
Dmytro <srk90512@gmail.com>
2024-02-11 22:46:25 +0200

    register and call search.js and search.css

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -32,12 +32,17 @@ class Assets
         wp_register_style('slick-css', AQUILA_BUILD_LIB_URI . '/css/slick.css', [], false, 'all');
         wp_register_style('slick-theme-css', AQUILA_BUILD_LIB_URI . '/css/slick-theme.css', ['slick-css'], false, 'all');
         wp_register_style('main-css', AQUILA_BUILD_CSS_URI . '/main.css', ['bootstrap-css'], filemtime(AQUILA_BUILD_CSS_DIR_PATH . '/main.css'), 'all');
+        wp_register_style('search-css', AQUILA_BUILD_CSS_URI . '/search.css', [], filemtime(AQUILA_BUILD_CSS_DIR_PATH . '/search.css'), 'all');
 
         // Enqueue Styles
         wp_enqueue_style('bootstrap-css');
         wp_enqueue_style('slick-css');
         wp_enqueue_style('slick-theme-css');
         wp_enqueue_style('main-css');
+
+        if(is_page('search')) {
+            wp_enqueue_style('search-css');
+        }
     }
 
     public function set_wp_scripts()
@@ -47,6 +52,7 @@ class Assets
         wp_register_script('main-js', AQUILA_BUILD_JS_URI . '/main.js', ['jquery', 'slick-js'], filemtime(AQUILA_BUILD_JS_DIR_PATH . '/main.js'), true);
         wp_register_script('bootstrap-js', AQUILA_BUILD_LIB_URI . '/js/bootstrap.bundle.min.js', ['jquery'], false, true);
         wp_register_script('author-js', AQUILA_BUILD_JS_URI . '/author.js', ['jquery'], filemtime(AQUILA_BUILD_JS_DIR_PATH . '/author.js'), true);
+        wp_register_script('search-js', AQUILA_BUILD_JS_URI . '/search.js', ['main-js'], filemtime(AQUILA_BUILD_JS_DIR_PATH . '/search.js'), true);
 
         // Enqueue Scripts
         wp_enqueue_script('main-js');
@@ -55,6 +61,21 @@ class Assets
 
         if (is_author()) {
             wp_enqueue_script('author-js');
+        }
+
+        // If search page.
+        if (is_page('search')) {
+            $filters_data = get_filters_data();
+            wp_enqueue_script('search-js');
+            wp_localize_script(
+                'search-js',
+                'search_settings',
+                [
+                    'rest_api_url' => home_url('/wp-json/af/v1/search'),
+                    'root_url' => home_url('search'),
+                    'filter_ids' => get_filter_ids($filters_data),
+                ]
+            );
         }
     }
 }
